6ce19db5affe227e7abe4e8251b2bf901679836d
Elijah Zimmerman <elijahz0409@gmail.com>
2026-03-27 12:27:46 -0400

    Remove shebang

--- tools/copy_license_header/copy_license_header.py
+++ tools/copy_license_header/copy_license_header.py
@@ -1,5 +1,3 @@
-#! .venv/bin python3
-#
 #  This file is part of Custodes SDC.
 #
 # Custodes SDC is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
